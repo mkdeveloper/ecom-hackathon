71ff19be7cbe44122ed863191bb3b6e68377d8a0
mkdeveloper <khubaib.mk@gmail.com>
2023-06-27 11:49:51 +0500

    fixed router import

--- src/components/shared/StartShopping.tsx
+++ src/components/shared/StartShopping.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const StartShopping = () => {
