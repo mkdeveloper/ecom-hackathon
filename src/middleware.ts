f6956951fc09a4123010d4d8ebddb7834192ba54
mkdeveloper <khubaib.mk@gmail.com>
2023-06-28 00:19:36 +0500

    fixed stripe webhooks connection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/products" ,"/female", "/male", "/kids", "/api/cart/:path*", "/studio/:path*"],
+  publicRoutes: ["/", "/products" ,"/female", "/male", "/kids", "/api/cart/:path*", "/api/webhooks/:path*", "/studio/:path*"],
 });
 
 export const config = {
